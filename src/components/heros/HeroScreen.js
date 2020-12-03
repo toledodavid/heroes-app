d2ee94203db584aa035d7487942b2444e76e4b71
David Toledo <dtoledoz0207@gmail.com>
2020-12-03 12:08:55 -0600

    get-params-from-url-in-HeroScreen-component

--- src/components/heros/HeroScreen.js
+++ src/components/heros/HeroScreen.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Redirect, useParams } from 'react-router-dom';
+import { getHeroById } from '../../selectors/getHeroById';
 
 
 const HeroScreen = () => {
+
+  const {heroId} = useParams();
+  
+  const hero = getHeroById(heroId);
+
+  if (!hero) {
+    return <Redirect to="/" />
+  }
+
+  const {superhero, publisher, alter_ego, first_appearance, characters} = hero;
+
   return(
     <div>
       <h1>Hero Screen</h1>
