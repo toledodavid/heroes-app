f3995badd61ef83795be7cceca5493f266db5e3f
David Toledo <dtoledoz0207@gmail.com>
2020-12-03 12:51:32 -0600

    add-design-for-HeroScreen-component

--- src/components/heros/HeroScreen.js
+++ src/components/heros/HeroScreen.js
@@ -3,7 +3,7 @@ import { Redirect, useParams } from 'react-router-dom';
 import { getHeroById } from '../../selectors/getHeroById';
 
 
-const HeroScreen = () => {
+const HeroScreen = ({history}) => {
 
   const {heroId} = useParams();
   
@@ -15,9 +15,35 @@ const HeroScreen = () => {
 
   const {superhero, publisher, alter_ego, first_appearance, characters} = hero;
 
+  const handleReturn = () => {
+    if (history.length <= 2) {
+      history.push('/');
+    } else {
+      history.goBack();
+    }
+  }
+
   return(
-    <div>
-      <h1>Hero Screen</h1>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img src={`../assets/heroes/${heroId}.jpg`} alt={superhero} className="img-thumbnail" />
+      </div>
+
+      <div className="col-8">
+        <h3>{superhero}</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item"><b>Alter ego: </b>{alter_ego}</li>
+          <li className="list-group-item"><b>Publisher</b>{publisher}</li>
+          <li className="list-group-item"><b>First appearance</b>{first_appearance}</li>
+        </ul>
+
+        <h5>Characters</h5>
+        <p>{characters}</p>
+
+        <button className="btn btn-outline-info" onClick={handleReturn}>
+          Return
+        </button>
+      </div>
     </div>
   );
 }
