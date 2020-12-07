0dadf14e9635b0aa36c1965e55401fcd3879c411
David Toledo <dtoledoz0207@gmail.com>
2020-12-07 12:32:16 -0600

    use-useContext-in-LoginScreen-component-to-dispatch-an-action-and-save-user-name-in-state

--- src/HerosApp.js
+++ src/HerosApp.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { AuthContext } from './auth/AuthContext';
 import { authReducer } from './auth/authReducer';
 import AppRouter from './routers/AppRouter';
@@ -9,7 +9,11 @@ const init = () => {
 
 const HerosApp = () => {
 
-  const [user, dispatch] = useReducer(authReducer, {}, init)
+  const [user, dispatch] = useReducer(authReducer, {}, init);
+
+  useEffect(() => {
+    localStorage.setItem('user', JSON.stringify(user));
+  }, [user]);
 
   return(
     <AuthContext.Provider value={{user, dispatch}}>
--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../auth/AuthContext';
+import { types } from '../../types/types';
 
 
 
 const LoginScreen = ({history}) => {
 
+  const { dispatch } = useContext(AuthContext);
+
   const handleLogin = () => {
     // history.push('/');
+    
+    dispatch({
+      type: types.login,
+      payload: {
+        name: 'David'
+      }
+    });
+
     history.replace('/');
+
   }
 
   return(
